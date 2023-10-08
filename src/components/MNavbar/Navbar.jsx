2ef790289ee0ea4a23d023bfac1544afb1fa8146
amarNrddn <amarnrddn02@gmail.com>
2023-10-09 00:24:53 +0700

    update: search bar and page move

--- src/components/MNavbar/Navbar.jsx
+++ src/components/MNavbar/Navbar.jsx
@@ -6,13 +6,20 @@ import { avatar } from '../../assets'
 
 const Navbar = () => {
   const navigate = useNavigate()
-  const [search, setSearch] = useState(false)
   const [inputSearch, setInputSearch] = useState({ name: 'searchUser' })
 
   const onChangeSearch = (e) => {
     setInputSearch({ ...inputSearch, [e.target.name]: e.target.value })
-    console.log(inputSearch)
   }
+
+  const onSubmit = (e) => {
+    if (e.key === 'Enter') {
+      console.log(inputSearch)
+    } 
+    console.log(inputSearch) 
+  }
+
+  
 
   return (
     <nav className='w-full flex justify-between bg-dark py-3 px-5 fixed z-50 '>
@@ -44,18 +51,18 @@ const Navbar = () => {
       </div>
 
       <div className="flex gap-3 cursor-pointer text-white text-[20px] font-bold items-center">
-        {search && (
-          <input
-            name='searchUser'
-            onChange={onChangeSearch}
-            type="text"
-            placeholder='search...'
-            className='outline-none rounded-sm text-[14px] pl-2 py-1 font-normal bg-dark opacity-1 border-[1px] border-white'
-          />
-        )}
-        <AiOutlineSearch 
-        className='font-bold text-[25px]'
-        onClick={() => setSearch(!search)} 
+        <input
+          name='searchUser'
+          onChange={onChangeSearch}
+          onKeyPress={onSubmit}
+          type="text"
+          placeholder='search...'
+          className='outline-none rounded-sm text-[14px] pl-2 py-1 font-normal bg-dark opacity-1 border-[1px] border-white'
+        />
+
+        <AiOutlineSearch
+          className='font-bold text-[25px]'
+          onClick={onSubmit}
         />
         <IoIosNotificationsOutline className='font-bold text-[25px]' />
         <div className="w-[40px] h-[40px]">
--- src/pages/MMove/MMove.jsx
+++ src/pages/MMove/MMove.jsx
@@ -1,15 +1,74 @@
-
-import React from 'react'
+import React, { useState } from 'react'
 import { useContext } from 'react'
 import { Contex } from '../../utils/fetch'
+import MSkeleton from '../../components/MSkeleton/MSkeleton'
+import { Link } from 'react-router-dom'
+import { AiOutlinePlayCircle } from 'react-icons/ai'
+import { CircularProgressbar } from 'react-circular-progressbar'
+import { config } from '../../config'
 
 const MMove = () => {
-  const { move } = useContext(Contex)
-  console.log(move)
-  
+  const { move, isLoading } = useContext(Contex)
+  const [customColor] = useState({
+    Up: '#16a34a',
+    midle: '#facc15',
+    low: '#e11d48'
+  })
+
   return (
-    <div className='pt-[5rem]'>
-      <h1 className='text-black'>afsdasdas</h1>
+    <div className='w-[100%] bg-dark text-white px-5 pt-[5rem] grid grid-cols-5 gap-2'>
+      {move.map((move, i) => {
+        const vote = Math.floor(move.vote_average * 10)
+        const percentage = vote;
+
+        const dateString = move.release_date || move.first_air_date
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const date = new Date(dateString);
+        const formattedDate = date.toLocaleDateString(undefined, options);
+
+        return (
+          <div key={i}>
+            {isLoading ? (
+              <MSkeleton />
+            ) : (
+              <div className="border border-neutral-800 shadow max-w-[260px] pb-3 relative rounded-md cursor-pointer flex flex-col overflow-hidden">
+                <div className="w-full rounded-md relative overflow-hidden transition ease-in-out  delay-150  hover:opacity-25 hover:scale-110 ">
+                  <img
+                    src={`${config.api_path_img}/${move.poster_path}`}
+                    alt="error"
+                    className='rounded-sm '
+                  />
+                  <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 ease-in-out  delay-150">
+                    <Link to={`${move.id}`} >
+                      <AiOutlinePlayCircle
+                        className='text-[60px]'
+                      />
+                    </Link>
+                  </div>
+                </div>
+                <div className="w-full mt-2 px-2 text white flex ">
+                  <div className="w-full">
+                    <p className=" truncate text-[16px] text-[#fff] font-bold max-w-[120px] rounded-sm">{move.title || move.name}</p>
+                  </div>
+                  <div className="w-[40px] h-[40px] absolute right-2 top-[80%] ">
+                    <CircularProgressbar
+                      className='w-[40px] text-[20px] font-bold h-[40px] '
+                      value={percentage}
+                      text={`${percentage}%`}
+                      styles={{
+                        path: {
+                          stroke: vote >= 80 ? customColor.Up : customColor.Up && vote <= 60 ? customColor.low : customColor.low && vote <= 50 ? customColor.midle : customColor.midle,
+                        },
+                      }}
+                    />
+                  </div>
+                </div>
+                <p className="max-w-[150px] mb-1 px-2  truncate rounded-sm text-[11px] text-slate-500 ">{formattedDate}</p>
+              </div>
+            )}
+          </div>
+        )
+      })}
     </div>
   )
 }
